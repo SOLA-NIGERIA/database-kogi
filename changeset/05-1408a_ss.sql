e98ae3f69eeddd6de864a1e3b64235f2745ae930
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-08-11 18:14:38 +0200

    new approach for getting the localized values and files for different states

---
+++ changeset/05-1408a_ss.sql
@@ -0,0 +1,5 @@
+﻿insert into system.setting(name, vl, active, description) values('state', 'Kogi', true, 'the state');
+insert into system.setting(name, vl, active, description) values('featureFloatFront', 'images/sola/front_float.svg', true, 'svg for the floating element in front page');
+insert into system.setting(name, vl, active, description) values('featureFloatBack', 'images/sola/back_float.svg', true, 'svg for the floating element in back page');
+insert into system.setting(name, vl, active, description) values('featureFront', 'images/sola/front.svg', true, 'svg for the background element in front page');
+insert into system.setting(name, vl, active, description) values('featureBack', 'images/sola/back.svg', true, 'svg for the background element in back page');
